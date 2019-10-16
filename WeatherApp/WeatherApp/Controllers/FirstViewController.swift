21670bfcd0a953b08b4b7c5dfb036ae36996fd2f
karyRmartinez <karymartinez@pursuit.org>
2019-10-16 12:29:07 -0400

    scenedelegate population

---
+++ WeatherApp/WeatherApp/Controllers/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  WeatherApp
+//
+//  Created by Kary Martinez on 10/16/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+}
