a9e4460181ccc5431d126a6d3f02a242d521972e
karyRmartinez <karymartinez@pursuit.org>
2019-10-16 15:55:24 -0400

    weather model

--- WeatherApp/WeatherApp/Controllers/persistence/networkHelper/Models/weatherStruct.swift
+++ WeatherApp/WeatherApp/Controllers/persistence/networkHelper/Models/weatherStruct.swift
@@ -10,5 +10,26 @@ import Foundation
 
 
 struct Weather: Codable {
+    let latitude: Double
+    let longitude: Double
+    let timezone: String
+    let currently: [currentlyWrapper]
     
+}
+
+struct currentlyWrapper: Codable {
+    let time: Int
+    let summary: String
+    let icon: String
+    let nearestStormDistance: Int?
+    let precipIntensity: Double
+    let precipIntensityError: Double?
+    let precipProbability: Double
+    let precipType: String?
+    let temperature, apparentTemperature, dewPoint, humidity: Double
+    let pressure, windSpeed, windGust: Double
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let visibility, ozone: Double
 }
